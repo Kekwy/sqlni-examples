135086a572a1c6fc2542dd7ad0007f7e92705519
Kekwy <keekkewy@qq.com>
2024-01-12 16:33:46 +0800

    pass testcase #1

--- src/main/java/com/kekwy/mybatis/sqlni/examples/entity/Employee.java
+++ src/main/java/com/kekwy/mybatis/sqlni/examples/entity/Employee.java
@@ -1,5 +1,9 @@
 package com.kekwy.mybatis.sqlni.examples.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * description
  *
@@ -7,6 +11,18 @@ package com.kekwy.mybatis.sqlni.examples.entity;
  * @version 1.0
  * @since 2024/1/11 16:31
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    private Integer eid;
+
+    private String department;
+
+    private String name;
+
+    private String type;
 
 }
